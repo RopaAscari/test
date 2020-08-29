c35354e089ab5a3ae5207931bf3172d00f534670
Danyel  Roper <droper@realdecoy.com>
2020-08-29 17:13:28 -0500

    kkk

---
+++ dist/precache-manifest.a25e13850de7e791e84639882342866a.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "f45e0539dec4026a73fe",
+    "url": "/css/app.126ac109.css"
+  },
+  {
+    "revision": "f45e0539dec4026a73fe",
+    "url": "/js/app.1088ab9f.js"
+  },
+  {
+    "revision": "bccc2819487b31a85101",
+    "url": "/js/chunk-2d0b1684.1554278b.js"
+  },
+  {
+    "revision": "d04c380659d82b18286c",
+    "url": "/js/chunk-2d0c49f5.9aeb58bd.js"
+  },
+  {
+    "revision": "92e54ea69b3cabd5a38e",
+    "url": "/css/chunk-3a2e8bfc.0e433876.css"
+  },
+  {
+    "revision": "92e54ea69b3cabd5a38e",
+    "url": "/js/chunk-3a2e8bfc.dbfb7b58.js"
+  },
+  {
+    "revision": "bf158335ae76981374f1",
+    "url": "/css/chunk-426e7aab.0e433876.css"
+  },
+  {
+    "revision": "bf158335ae76981374f1",
+    "url": "/js/chunk-426e7aab.98830426.js"
+  },
+  {
+    "revision": "7f2c0046c274c463fb05",
+    "url": "/css/chunk-76ea9e48.0e433876.css"
+  },
+  {
+    "revision": "7f2c0046c274c463fb05",
+    "url": "/js/chunk-76ea9e48.5be9486b.js"
+  },
+  {
+    "revision": "21b594c631028fe1a084",
+    "url": "/css/hello-world.9c8440df.css"
+  },
+  {
+    "revision": "21b594c631028fe1a084",
+    "url": "/js/hello-world.88fa041c.js"
+  },
+  {
+    "revision": "dc64860dbfe7710c11d0",
+    "url": "/js/runtime.4419dbd2.js"
+  },
+  {
+    "revision": "33bd84daa14420acd628",
+    "url": "/js/vendor.fec92474.js"
+  },
+  {
+    "revision": "b4506c99fe5f9844f9e63b4363c32a32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc2b73a170af1aaf88cfda4008591841",
+    "url": "/favicon 32 .png"
+  },
+  {
+    "revision": "822758a6efa8655f5c0d3ae229917153",
+    "url": "/pwa-icons/favicon 114 .png"
+  },
+  {
+    "revision": "ee5e084d0be97fab27a1e0d089cd7c84",
+    "url": "/pwa-icons/favicon 120 .png"
+  },
+  {
+    "revision": "d98cab7ba6c1ea4315edc572e72358cd",
+    "url": "/pwa-icons/favicon 128 .png"
+  },
+  {
+    "revision": "a07e7c2174a358ce00be95468288af31",
+    "url": "/pwa-icons/favicon 144 .png"
+  },
+  {
+    "revision": "56da7b5e87cc9d9cadb67112b4b74000",
+    "url": "/pwa-icons/favicon 152 .png"
+  },
+  {
+    "revision": "fd0b35acba104e74f4f8429cd95aeb14",
+    "url": "/pwa-icons/favicon 192 .png"
+  },
+  {
+    "revision": "6d49d2b4c66149599c0a846a9c85588e",
+    "url": "/pwa-icons/favicon 256 .png"
+  },
+  {
+    "revision": "c9ae02bcd9aa4a8d2824e3777c95d34a",
+    "url": "/pwa-icons/favicon 48 .png"
+  },
+  {
+    "revision": "0200c9d6be4d289bc30d84b726bb3809",
+    "url": "/pwa-icons/favicon 180 .png"
+  },
+  {
+    "revision": "bc2b73a170af1aaf88cfda4008591841",
+    "url": "/pwa-icons/favicon 32 .png"
+  },
+  {
+    "revision": "7732f222b18f47dc16befde9395f9174",
+    "url": "/pwa-icons/favicon 512 .png"
+  },
+  {
+    "revision": "c6f575e56ad78246c88ee6b2b1b07d10",
+    "url": "/pwa-icons/favicon 60 .png"
+  },
+  {
+    "revision": "e4087bc9a6b642d59ac18b4861051556",
+    "url": "/pwa-icons/favicon 76 .png"
+  },
+  {
+    "revision": "ccf8e3704a60bf8f93f6ff05e6a5085f",
+    "url": "/pwa-icons/favicon 96 .png"
+  }
+];
