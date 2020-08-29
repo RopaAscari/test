cbc6958bf1f5b284f934661386f8c70eab9c9a92
Danyel  Roper <droper@realdecoy.com>
2020-08-29 16:49:31 -0500

    yeaaa

---
+++ src/pages/hello-world/hello-world.vue
@@ -0,0 +1,11 @@
+<style lang="scss" src="./hello-world.scss"></style>
+<script lang="ts" src="./hello-world.ts"></script>
+<template> 
+    <div>
+      <h3>Rise</h3>
+      <div class=".buttons">
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
+</template>
